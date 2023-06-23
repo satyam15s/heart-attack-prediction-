b074c4191e8295fa10858d4706c34c1fc24d9e8b
satyam15s <100987133+satyam15s@users.noreply.github.com>
2023-06-23 13:57:36 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+# Importing essential libraries
+from flask import Flask, render_template, request
+import pickle
+import numpy as np
+
+# Load the Random Forest CLassifier model
+filename = 'heart.pkl'
+model = pickle.load(open(filename, 'rb'))
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('main.html')
+
+
+@app.route('/predict', methods=['GET','POST'])
+def predict():
+    if request.method == 'POST':
+        age = int(request.form['age'])
+        sex = request.form.get('sex')
+
+      
+        
+        cp = request.form.get('cp')
+        trtbps = int(request.form['trestbps'])
+        chol = int(request.form['chol'])
+        fbs = request.form.get('fbs')
+        restecg = int(request.form['restecg'])
+        thalach = int(request.form['thalach'])
+        exang = request.form.get('exang')
+       
+        caa = int(request.form['ca'])
+        
+        
+        data = np.array([[age,sex,cp,trtbps,chol,fbs,restecg,thalach,exang,caa]])
+        my_prediction = model.predict(data)
+        
+        return render_template('result.html', prediction=my_prediction)
+        
+        
+
+if __name__ == '__main__':
+	app.run(debug=True)
